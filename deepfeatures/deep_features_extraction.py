0bacf897d383702b844f7a3ad120f862ae819388
Andrej Rosic <arosic@edu.uwaterloo.ca>
2018-04-21 13:31:18 -0400

    Updating deep features code to write to pandas dataframe

--- deepfeatures/deep_features_extraction.py
+++ deepfeatures/deep_features_extraction.py
@@ -1,44 +1,50 @@
-import csv
-import os
-
 from keras import applications
 from keras.applications.resnet50 import preprocess_input
-from keras.preprocessing import image
 import numpy as np
 
-def get_features(id, directory, eachFile):
-    model = applications.resnet50.ResNet50(weights='imagenet', include_top=False, pooling='avg')
-    print (eachFile)
-    img = image.load_img(directory + eachFile)
+import prepare_features
+
+
+def get_deep_feature(model, img):
 
     # convert image to numpy array
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    img = img.astype(np.float64)
+    img = np.expand_dims(img, axis=0)
+    preprocessed_img = preprocess_input(img)
 
     # extract the features
-    features = model.predict(x)[0]
+    features = model.predict(preprocessed_img)
 
-    # convert from numpy to a list of values
-    features_arr = np.char.mod('%f', features)
+    return features
 
-    # write to tsv file
-    with open('records.tsv', 'a') as tsvfile:
-        w = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
-        w.writerows([id, features_arr])
 
-    return (features_arr)
+def extract_deep_features(base_path=None, output_path='./img.w.deep.features.tab'):
 
-# Image Path directory
-strDir = '../photos/'
+    model = applications.resnet50.ResNet50(weights='imagenet', include_top=False, pooling='avg')
 
-# Iterate through all images in photos folder
-for file in os.listdir(os.fsencode(strDir)):
-    filename = os.fsdecode(file)
-    if filename.endswith(".jpg"):
-        id = filename.split('.')[0]
-        featList = get_features(id, strDir, filename)
-        print(featList)
+    df = prepare_features.load_descriptions(base_path)
+    df['deep_feature'] = None
 
-    else:
-        print ('no file found')
+    total_images = df.shape[0]
+    images_processed = 0
+    for idx, row in df.iterrows():
+        # Provide a status update if necessary
+        if images_processed % 100.0 == 0:
+            print(str(images_processed) + '/' + str(total_images) + ' images (i.e. ' +
+                  str(np.round(images_processed / total_images, 3) * 100) + "%) complete.")
+
+        # Extract LBP features for image
+        img = prepare_features.read_colour_img(row.img_path)
+        df.at[idx, 'deep_feature'] = get_deep_feature(model, img)
+        images_processed += 1
+
+    # Write the dataframe to disk
+    if output_path is not None:
+        df.to_csv(output_path, index=False, sep="\t")
+
+    return df
+
+
+if __name__ == '__main__':
+
+    extract_deep_features()
